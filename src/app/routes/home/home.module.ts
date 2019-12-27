55f8051858fe05f2c5cf1f0627924cbe76e2e431
SamaneMotahariNejad <s.motaharinejad@gmail.com>
2019-12-27 11:16:58 +0330

    new change

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SlideshowComponent } from './slideshow/slideshow.component';
 import { HomeComponent } from './home.component';
+import { SecondSectionComponent } from './second-section/second-section.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
@@ -17,8 +17,8 @@ const routes: Routes = [
     RouterModule.forChild(routes)
   ],
   declarations: [
-    SlideshowComponent,
-    HomeComponent
+    HomeComponent,
+    SecondSectionComponent
   ],
   exports: [
     RouterModule,
